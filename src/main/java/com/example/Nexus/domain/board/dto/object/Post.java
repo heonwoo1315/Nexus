0f052ce01a62d6db78f11010826e430945442b63
HeonWoo Park <phwoo1315@naver.com>
2026-01-26 11:59:01 +0900

    feat: Post DTO writerId, hits 컬럼 추가

--- src/main/java/com/example/Nexus/domain/board/dto/object/Post.java
+++ src/main/java/com/example/Nexus/domain/board/dto/object/Post.java
@@ -13,5 +13,8 @@ public class Post {
     private Long id;
     private String title;
     private String content;
+    private String writerId;
+    private Integer hits; // int -> Integer로 변경 그래야 데이터가 없을 때 null을 허용하고, 나중에 DB에서 0으로 자연스럽게 치환 가능
+    // PostControllerTest클래스의 createPostApiTest()에서 보내는 JSON에 hits가 없으므로 null로 넣어주는데 여기서 자료형 int는 null을 수용 불가능
     private LocalDateTime createdAt;
 }
