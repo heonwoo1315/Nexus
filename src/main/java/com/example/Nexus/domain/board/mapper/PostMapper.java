c14c3c0a9f4f3d8a8d80cd13fb791e49ecb3adfc
HeonWoo Park <phwoo1315@naver.com>
2026-01-26 11:59:47 +0900

    feat: 게시판에 필요한 메서드 정의와 그 메서드에 대응되는 쿼리문 작성

--- src/main/java/com/example/Nexus/domain/board/mapper/PostMapper.java
+++ src/main/java/com/example/Nexus/domain/board/mapper/PostMapper.java
@@ -2,8 +2,15 @@ package com.example.Nexus.domain.board.mapper;
 
 import com.example.Nexus.domain.board.dto.object.Post;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import java.util.List;
 
 @Mapper
 public interface PostMapper {
     void insert(Post post);
+    List<Post> findAll(@Param("offset") int offset, @Param("size") int size);
+    Post findById(Long id);
+    void updateHits(Long id);
+    void update(Post post);
+    void delete(Long id);
 }
